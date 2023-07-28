a4206524866faf277fe7613fabe4ca67957f0a17
Beluhga <michaelfernando@live.com>
2023-07-28 17:27:04 -0300

    armazenando o refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,70 @@
+import { storageAuthTokenGet } from '@storage/storageAuthToken';
 import { AppError } from '@utils/AppError';
-import axios from 'axios';
+import axios, {AxiosError, AxiosInstance} from 'axios';
+
+type SignOut = () => void;
+
+type PromiseType = {
+    onSuccess: (token: string) => void;
+    onFailure: (error: AxiosError) => void;
+}
+
+type APIInstanceProps = AxiosInstance & {
+    registerInterceptTokenManager: (signOut: SignOut ) => () => void; // interceptação da resposta
+}
 
 const api = axios.create({
     baseURL: 'http://192.168.0.8:3333',
     timeout: 1000,
-});
+}) as APIInstanceProps;
 
-api.interceptors.response.use(response => response, error => {
-    if(error.response && error.response.data){
-        return Promise.reject(new AppError(error.response.data.message))
-    } else {
-        return Promise.reject(error);
-    }
-}); // resposta = acessa todas as informções da requisição que esta sendo feita. error = pra quando der errado oq irar ser feito
+let feiledQueue: Array<PromiseType> = [];
+let isRefreshing = false;
+
+api.registerInterceptTokenManager = signOut => {
+    const interceptTokenManager = api.interceptors.response.use(response => response, async (RequestError) => {
+        if(RequestError?.response?.status === 401){ // status de não autorizado
+            if(RequestError.response.data?.message === 'token.expired' || RequestError.response.data?.message === 'token.invalid'){ // se o token da invalido ou expirado
+                const {refresh_token} = await storageAuthTokenGet(); // reculpera o refresh_token, que esta armazenada no dispositivo(storageAuthTokenGet())
+
+                if(!refresh_token){
+                    signOut(); // para deslogar caso nao tenha o refresh_token
+                    return Promise.reject(RequestError) // para rejeitar e devolver o requestError
+                }
+                const configuracaoDaRequisicaoOriginal = RequestError.config;
+
+                if(isRefreshing){
+                    return new Promise((resolve, reject) => {
+                        feiledQueue.push({
+                            onSuccess: (token: string) => {
+                                configuracaoDaRequisicaoOriginal.headers = {'authorization' : `Bearer ${token}`}
+                            },
+                            onFailure: () => {},
+                        })
+                    })
+                 }
+
+                isRefreshing = true
+                
+            }
+
+            signOut(); // fluxo novo de autenticação
+        }
+
+
+
+        if(RequestError.response && RequestError.response.data){
+            return Promise.reject(new AppError(RequestError.response.data.message))
+        } else {
+            return Promise.reject(RequestError);
+        }
+    }); // resposta = acessa todas as informções da requisição que esta sendo feita. error = pra quando der errado oq irar ser feito
+
+    return ( ) => {
+        api.interceptors.response.eject(interceptTokenManager); // para fazer a interceptacao, responde e ejetae o "interceptTokenManager" 
+    };  
+};
+
 
 export {api};
 
--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -2,14 +2,21 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { AUTH_TOKEN_STORAGE } from "./storageConfig";
 
-export async function storageAuthTokenSave(token: string) {
-    await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token);
+type storageAuthTokenProps = {
+    token: string;
+    refresh_token: string; // ele serve para gerar um novo token atualizado
+}
+
+export async function storageAuthTokenSave({token, refresh_token} : storageAuthTokenProps) {
+    await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, JSON.stringify({token, refresh_token})); //JSON.stringify({token, refresh_token}) pq o {token, refresh_token} é são objetos 
 }
 
 export async function storageAuthTokenGet(){
-    const token = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE);
+    const response = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE);
 
-    return token;
+    const {token, refresh_token} : storageAuthTokenProps = response ? JSON.parse(response) : {}; // JSON.parse(response) convertendo para um objeto
+
+    return {token, refresh_token};
 }
 export async function storageAuthTokenRemove(){
     await AsyncStorage.removeItem(AUTH_TOKEN_STORAGE);
